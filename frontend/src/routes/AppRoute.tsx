be2926ad39b6a7eb07116cace0e3a46f56b7e955
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-08-04 00:54:18 +0700

    update approute

--- frontend/src/routes/AppRoute.tsx
+++ frontend/src/routes/AppRoute.tsx
@@ -1,11 +1,12 @@
-import { Routes } from "react-router-dom";
+// import { Routes } from "react-router-dom";
 
 const AppRoute = () => {
   return (
     <>
-      <Routes>
+      {/* <Routes>
        
-      </Routes>
+      </Routes> */}
+      <p className="text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quos laboriosam dolorem corrupti nisi autem ipsa culpa possimus quibusdam maxime vero suscipit, eveniet eaque. Impedit deserunt ut quod incidunt aperiam.</p>
     </>
   );
 };
