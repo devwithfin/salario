fdb0ae6ad746d34148e9459e06ffa50f366f2a70
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-08-05 10:52:40 +0700

    add retry mechanism for db connection

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,27 @@ const sequelize = require("./config/db");
 
 const PORT = process.env.PORT;
 
-sequelize.authenticate().then(() => {
-    console.log("Database connected");
-    app.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}/`);
-    });
-  })
-  .catch((err) => {
-    console.log("DB connection error:", err);
-  });
+ async function connectWithRetry(retries = 10) {
+  while (retries) {
+    try {
+      await sequelize.authenticate();
+      console.log("Database connected");
+
+      app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}/`);
+      });
+      break;
+    } catch (err) {
+      console.log(`DB not ready. Retrying in 5s... (${retries} retries left)`);
+      retries--;
+      await new Promise(res => setTimeout(res, 5000));
+    }
+  }
+
+  if (retries === 0) {
+    console.error("Failed to connect to DB after multiple attempts.");
+    process.exit(1);
+  }
+}
+
+connectWithRetry();
